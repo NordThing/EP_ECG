048793d32e4872b7cae68c42b1b5aa75c5d202da
Henrik Allberg <33314486+Henke1983@users.noreply.github.com>
2022-10-09 20:08:45 +0200

    Rename ANALBEAT.CPP to analbeat.cpp

--- analbeat.cpp
+++ analbeat.cpp
@@ -1,5 +1,14 @@
 /*****************************************************************************
-FILE:  analbeat.cppAUTHOR:	Patrick S. HamiltonREVISED:	5/13/2002  ___________________________________________________________________________analbeat.cpp: Analyze BeatCopywrite (C) 2001 Patrick S. HamiltonThis file is free software; you can redistribute it and/or modify it underthe terms of the GNU Library General Public License as published by the Free
+FILE:  analbeat.cpp
+AUTHOR:	Patrick S. Hamilton
+REVISED:	5/13/2002
+  ___________________________________________________________________________
+
+analbeat.cpp: Analyze Beat
+Copywrite (C) 2001 Patrick S. Hamilton
+
+This file is free software; you can redistribute it and/or modify it under
+the terms of the GNU Library General Public License as published by the Free
 Software Foundation; either version 2 of the License, or (at your option) any
 later version.
 
