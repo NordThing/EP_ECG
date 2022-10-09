c3d886ce679a5d44015253d72192dcff92b717c4
Henrik Allberg <33314486+Henke1983@users.noreply.github.com>
2022-10-09 20:12:30 +0200

    Rename POSTCLAS.CPP to postclas.cpp

--- postclas.cpp
+++ postclas.cpp
@@ -1,5 +1,45 @@
 /*****************************************************************************
-FILE:  postclas.cppAUTHOR:	Patrick S. HamiltonREVISED:	5/13/2002  ___________________________________________________________________________postclas.cpp: Post classifierCopywrite (C) 2002 Patrick S. HamiltonThis file is free software; you can redistribute it and/or modify it underthe terms of the GNU Library General Public License as published by the FreeSoftware Foundation; either version 2 of the License, or (at your option) anylater version.This software is distributed in the hope that it will be useful, but WITHOUT ANYWARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR APARTICULAR PURPOSE.  See the GNU Library General Public License for moredetails.You should have received a copy of the GNU Library General Public License alongwith this library; if not, write to the Free Software Foundation, Inc., 59Temple Place - Suite 330, Boston, MA 02111-1307, USA.You may contact the author by e-mail (pat@eplimited.edu) or postal mail(Patrick Hamilton, E.P. Limited, 35 Medford St., Suite 204 Somerville,MA 02143 USA).  For updates to this software, please visit our website(http://www.eplimited.com).  __________________________________________________________________________This file contains functions for classifying beats based after thefollowing beat is detected.	ResetPostClassify() -- Resets static variables used by		PostClassify()	PostClassify() -- classifies each beat based on six preceding		beats and the following beat.	CheckPostClass() --  classifys beat type based on the last		eight post classifications of that beat.	CheckPCRhythm() -- returns the classification of the RR interval		for this type of beat based its previous eight RR intervals.****************************************************************/
+FILE:  postclas.cpp
+AUTHOR:	Patrick S. Hamilton
+REVISED:	5/13/2002
+  ___________________________________________________________________________
+
+postclas.cpp: Post classifier
+Copywrite (C) 2002 Patrick S. Hamilton
+
+This file is free software; you can redistribute it and/or modify it under
+the terms of the GNU Library General Public License as published by the Free
+Software Foundation; either version 2 of the License, or (at your option) any
+later version.
+
+This software is distributed in the hope that it will be useful, but WITHOUT ANY
+WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A
+PARTICULAR PURPOSE.  See the GNU Library General Public License for more
+details.
+
+You should have received a copy of the GNU Library General Public License along
+with this library; if not, write to the Free Software Foundation, Inc., 59
+Temple Place - Suite 330, Boston, MA 02111-1307, USA.
+
+You may contact the author by e-mail (pat@eplimited.edu) or postal mail
+(Patrick Hamilton, E.P. Limited, 35 Medford St., Suite 204 Somerville,
+MA 02143 USA).  For updates to this software, please visit our website
+(http://www.eplimited.com).
+  __________________________________________________________________________
+
+This file contains functions for classifying beats based after the
+following beat is detected.
+
+	ResetPostClassify() -- Resets static variables used by
+		PostClassify()
+	PostClassify() -- classifies each beat based on six preceding
+		beats and the following beat.
+	CheckPostClass() --  classifys beat type based on the last
+		eight post classifications of that beat.
+	CheckPCRhythm() -- returns the classification of the RR interval
+		for this type of beat based its previous eight RR intervals.
+
+****************************************************************/
 
 #include "bdac.h"
 #include "ecgcodes.h"
