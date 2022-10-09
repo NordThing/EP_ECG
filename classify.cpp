87d8cab78e0138fd38649400fa94f7161feece48
Henrik Allberg <33314486+Henke1983@users.noreply.github.com>
2022-10-09 20:10:40 +0200

    Rename CLASSIFY.CPP to classify.cpp

--- classify.cpp
+++ classify.cpp
@@ -1,5 +1,14 @@
 /*****************************************************************************
-FILE:  classify.cppAUTHOR:	Patrick S. HamiltonREVISED:	5/13/2001  ___________________________________________________________________________classify.cpp: Classify a given beat.Copywrite (C) 2001 Patrick S. HamiltonThis file is free software; you can redistribute it and/or modify it underthe terms of the GNU Library General Public License as published by the Free
+FILE:  classify.cpp
+AUTHOR:	Patrick S. Hamilton
+REVISED:	5/13/2001
+  ___________________________________________________________________________
+
+classify.cpp: Classify a given beat.
+Copywrite (C) 2001 Patrick S. Hamilton
+
+This file is free software; you can redistribute it and/or modify it under
+the terms of the GNU Library General Public License as published by the Free
 Software Foundation; either version 2 of the License, or (at your option) any
 later version.
 
