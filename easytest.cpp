f7ce82f397a84e5c20ec3675dc667ff8a79b3ee6
Henrik Allberg <33314486+Henke1983@users.noreply.github.com>
2022-10-09 22:18:45 +0200

    Update easytest.cpp

--- easytest.cpp
+++ easytest.cpp
@@ -69,26 +69,10 @@ This file must be linked with object files produced from:
 #include "stdio.h"
 #include "qrsdet.h"		// For sample rate.
 
-#define MITDB				// Comment this line out to process AHA data.
-#ifdef MITDB
-#define ECG_DB_PATH	"C:\\MITDB\\"	// Path to where MIT/BIH data.
-#define REC_COUNT	48
-int Records[REC_COUNT] = {100,101,102,103,104,105,106,107,108,109,111,112,
-							113,114,115,116,117,118,119,121,122,123,124,
-							200,201,202,203,205,207,208,209,210,212,213,214,
-							215,217,219,220,221,222,223,228,230,231,232,233,234} ;
+#include "inputs.h"
 
-#else
-#define ECG_DB_PATH	"C:\\AHADAT~1\\"	// Path to where AHA data.
-#define REC_COUNT	69
-int Records[REC_COUNT] = {1201,1202,1203,1204,1205,1206,1207,1208,1209,1210,
-									2201,2203,2204,2205,2206,2207,2208,2209,2210,
-									3201,3202,3203,3204,3205,3206,3207,3208,3209,3210,
-									4201,4202,4203,4204,4205,4206,4207,4208,4209,4210,
-									5201,5202,5203,5204,5205,5206,5207,5208,5209,5210,
-									6201,6202,6203,6204,6205,6206,6207,6208,6209,6210,
-									7201,7202,7203,7204,7205,7206,7207,7208,7209,7210} ;
-#endif
+#include "bdac.h"
+
 // External function prototypes.
 void ResetBDAC(void) ;
 int BeatDetectAndClassify(int ecgSample, int *beatType, int *beatMatch) ;
